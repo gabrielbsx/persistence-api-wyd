36917e338d078ed9551de781a0db6a35fbfb97b7
Gabriel Barbosa <gabrielturing@gmail.com>
2023-02-22 01:29:54 -0300

    feat: add c++ wrapper for impot-user and import-password

---
+++ wrapper/main.cpp
@@ -0,0 +1,46 @@
+#include "iostream"
+#include "string"
+#include "filesystem"
+#include "fstream"
+
+void readAndExecuteImportFilesOnFolder(std::string folder, std::string importType)
+{
+  for (const auto &entry : std::filesystem::directory_iterator(folder))
+  {
+    std::ifstream importFile(entry.path());
+    if (!importFile.is_open())
+    {
+      std::cout << "Error opening file" << entry.path() << std::endl;
+      continue;
+    }
+    std::string importText((std::istreambuf_iterator<char>(importFile)), std::istreambuf_iterator<char>());
+    importFile.close();
+    if (importText.empty())
+    {
+      std::cout << "File is empty" << entry.path() << std::endl;
+      continue;
+    }
+    std::istringstream iss(importText);
+    std::string username, password;
+    iss >> username >> password;
+    std::cout << username << " " << password << std::endl;
+    if (importType == "import-password")
+    {
+    }
+    else if (importType == "import-user")
+    {
+    }
+    else
+    {
+      std::cout << "Unknown import type" << std::endl;
+    }
+    return;
+  }
+}
+
+int main()
+{
+  const auto importUserFolder = std::filesystem::current_path() / "../" / "../" / "mock/server/dbsrv/run/import-password";
+  readAndExecuteImportFilesOnFolder(importUserFolder, "import-password");
+  return 0;
+}
