92bce9b40bd466a6136cf2e737a71a15e1f576a3
Gabriel Barbosa <gabrielturing@gmail.com>
2023-02-26 14:31:11 -0300

    refactor: add flag if import user and pass is not used

--- src/adapters/data-sources/filesystem/repositories/user-repository.ts
+++ src/adapters/data-sources/filesystem/repositories/user-repository.ts
@@ -1,4 +1,4 @@
-import { writeFileSync, existsSync } from 'node:fs'
+import { writeFileSync, existsSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { type User } from '../../../../core/domain/entities'
 import { type UserRepository } from '../../../../core/domain/repositories'
@@ -7,19 +7,39 @@ import { randomUUID } from 'node:crypto'
 
 export class FsUserRepository implements UserRepository {
   static _subDirectoriesAccount = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i)).concat(['etc'])
-  static _userDatabasePath = join(__dirname, '..', '..', '..', '..', '..', 'mock', 'server', 'dbsrv', 'run', 'accounts')
+  static _userDatabasePath = join(__dirname, '..', '..', '..', '..', '..', 'mock', 'server', 'dbsrv', 'run', 'account')
   static _importUser = join(__dirname, '..', '..', '..', '..', '..', 'mock', 'server', 'dbsrv', 'run', 'import-user')
   static _importPassword = join(__dirname, '..', '..', '..', '..', '..', 'mock', 'server', 'dbsrv', 'run', 'import-password')
+  static _baseAccount = join(__dirname, '..', '..', '..', '..', '..', 'base')
+  static _isBaseAccountUsed = true
 
   async create (user: Pick<User, 'username' | 'password'>): Promise<boolean> {
-    const randomFileName = `${randomUUID()}.txt`
-    writeFileSync(join(FsUserRepository._importUser, randomFileName), `${user.username} ${user.password}`)
+    if (R.not(FsUserRepository._isBaseAccountUsed)) {
+      const randomFileName = `${randomUUID()}.txt`
+      writeFileSync(join(FsUserRepository._importUser, randomFileName), `${user.username} ${user.password}`)
+      return true
+    }
+    const baseBuffer = readFileSync(FsUserRepository._baseAccount)
+    baseBuffer.write(user.username, 0, 16)
+    baseBuffer.write(user.password, 16, 32)
+    const subDirectoryByUsername = (user.username[0].match(/[a-z]/i) !== null) ? user.username[0] : 'etc'
+    const directoryUser = join(FsUserRepository._userDatabasePath, subDirectoryByUsername, user.username)
+    writeFileSync(directoryUser, baseBuffer)
     return true
   }
 
   async update (user: Pick<User, 'username' | 'password'>): Promise<boolean> {
-    const randomFileName = `${randomUUID()}.txt`
-    writeFileSync(join(FsUserRepository._importPassword, randomFileName), `${user.username} ${user.password}`)
+    if (R.not(FsUserRepository._isBaseAccountUsed)) {
+      const randomFileName = `${randomUUID()}.txt`
+      writeFileSync(join(FsUserRepository._importPassword, randomFileName), `${user.username} ${user.password}`)
+      return true
+    }
+    const subDirectoryByUsername = (user.username[0].match(/[a-z]/i) !== null) ? user.username[0] : 'etc'
+    const accountPath = join(FsUserRepository._userDatabasePath, subDirectoryByUsername, user.username)
+    const accountBuffer = readFileSync(accountPath)
+    accountBuffer.write(user.password, 16, user.password.length)
+    accountBuffer.writeUInt8(0x00, 16 + user.password.length)
+    writeFileSync(accountPath, accountBuffer)
     return true
   }
 
